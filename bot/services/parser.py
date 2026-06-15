66be86d509f099d8410383d13c4754d477c1bb55
Yaroslav <x451F@proton.me>
2026-06-15 15:16:21 +0200

    Refactor parse_word_page function to improve variable naming and dynamic grammar extraction

--- bot/services/parser.py
+++ bot/services/parser.py
@@ -4,23 +4,36 @@ from bs4 import BeautifulSoup
 
 def parse_word_page(html, query, source_url):
     soup = BeautifulSoup(html, 'html.parser')
-    container = soup.select_one('div.featured-box.featured-box-primary')
-    if container is None:
+    article = soup.select_one('div.featured-box.featured-box-primary')
+    if article is None:
         return None
 
-    title_tag = container.select_one("h4.text-uppercase")
+    title_tag = article.select_one("h4.text-uppercase")
     if title_tag is None:
         return None
 
     title = title_tag.get_text(strip=True)
 
+    grammar_tag = article.select_one("section.orfo")
+    if grammar_tag is None:
+        return None
+
+    grammar = grammar_tag.get_text(" ", strip=True)
+    print("\nTITLE:", title)
+    print("GRAMMAR:", grammar)
+
+    definition_tags = article.select("div.toggle.toggle-sum .toggle-content p")
+
+    definitions = [tag.get_text(" ", strip=True) for tag in definition_tags]
+    print("DEFINITIONS:", definitions)
+
     return WordResult(
         query=query,
         title=title,
-        grammar="іменник, жіночий рід, неістота",
+        grammar=grammar,
         short_text='',
         full_text=None,
         is_truncated=False,
         source_url=source_url,
-        definitions=['Definition 1', 'Definition 2']
+        definitions=definitions
     )
